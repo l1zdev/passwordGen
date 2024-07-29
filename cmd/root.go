140bd50610b40f058ab7348c89a7fcd7a76ae705
liz <dd.mxil333@gmail.com>
2024-07-29 23:40:26 +0200

    root.go

--- cmd/root.go
+++ cmd/root.go
@@ -15,13 +15,14 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "passwordGen",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "CLI app that generates customizable passwords",
+	Long: `This application was writen as a practice to get into golang
+	
+	
+	It generates custom passwords 
+	for example try:
+	passwordGen generate -l 12 -d -s
+	`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
